5680a477c54c8c8a8b5e25d662274787765c3ec3
jpzkskep <jpzkskep@gmail.com>
2025-08-03 14:55:08 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    int n;
+    long long fibo[20];
+    scanf("%d",&n);
+    fibo[0]=0;
+    fibo[1]=1;
+    for (int i=2; i<=n; i++) {
+        fibo[i]=fibo[i-1]+fibo[i-2];
+    }
+    printf("%lld",fibo[n]);
+    return 0;
+}
