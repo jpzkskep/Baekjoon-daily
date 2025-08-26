9aeb41223ba7a851163ec8ad17ba6522f02f8d39
jpzkskep <jpzkskep@gmail.com>
2025-08-26 19:47:05 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 64 ms, Memory: 2556 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b);
+
+typedef struct {
+    int x;
+    int y;
+} point;
+
+int main(void) {
+    point jum[100001];
+    int n;
+    scanf("%d",&n);
+    for (int i=0; i<n; i++) {
+        scanf("%d %d",&jum[i].x,&jum[i].y);
+    }
+    
+    qsort(jum,n,sizeof(point),compare);
+    
+    for (int i=0; i<n; i++) {
+        printf("%d %d\n",jum[i].x,jum[i].y);
+    }
+    
+    return 0;
+}
+
+int compare(const void*a,const void*b) {
+    point num1=*(point*)a;
+    point num2=*(point*)b;
+    if (num1.x>num2.x) {
+        return 1;
+    }
+    else if (num1.x<num2.x) {
+        return -1;
+    }
+    else if (num1.x==num2.x) {
+        if (num1.y>num2.y) {
+            return 1;
+        }
+        else if (num1.y<num2.y) {
+            return -1;
+        }
+    }
+    return 0;
+}
