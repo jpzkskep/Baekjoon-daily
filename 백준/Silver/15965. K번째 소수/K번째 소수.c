66c33894198aa131a6b348714b4c8e27033857bf
jpzkskep <jpzkskep@gmail.com>
2025-08-27 14:21:52 +0900

    [Silver II] Title: K번째 소수, Time: 1164 ms, Memory: 40056 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.c
+++ 백준/Silver/15965. K번째 소수/K번째 소수.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main(void) {
-    int num[500001];
+    int num[10000001];
     int k;
     int count=0;
     int NUM;
     scanf("%d",&k);
     
-    for (int i=2; i<=500000; i++) {
+    for (int i=2; i<=10000000; i++) {
         num[i]=1;
     }
-    for (int i=2; i<=500000; i++) {
-        for (int j=2*i; j<=500000; j+=i) {
+    for (int i=2; i<=10000000; i++) {
+        for (int j=2*i; j<=10000000; j+=i) {
             num[j]=0;
         }
     }
-    for (int i=2; i<=500000; i++) {
+    for (int i=2; i<=10000000; i++) {
         if (num[i]==1) {
             count++;
             NUM=i;
