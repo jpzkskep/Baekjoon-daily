62b820ad0476ecc4c08e747a0eee8bcc85a9e07a
jpzkskep <jpzkskep@gmail.com>
2025-09-25 09:43:02 +0900

    [Bronze III] Title: 학점계산, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char grade[5];
+    scanf("%s",grade);
+    if (strcmp(grade,"A+")==0) {
+        printf("4.3");
+    }
+    else if (strcmp(grade,"A0")==0) {
+        printf("4.0");
+    }
+    else if (strcmp(grade,"A-")==0) {
+        printf("3.7");
+    }
+    else if (strcmp(grade,"B+")==0) {
+        printf("3.3");
+    }
+    else if (strcmp(grade,"B0")==0) {
+        printf("3.0");
+    }
+    else if (strcmp(grade,"B-")==0) {
+        printf("2.7");
+    }
+    else if (strcmp(grade,"C+")==0) {
+        printf("2.3");
+    }
+    else if (strcmp(grade,"C0")==0) {
+        printf("2.0");
+    }
+    else if (strcmp(grade,"C-")==0) {
+        printf("1.7");
+    }
+    else if (strcmp(grade,"D+")==0) {
+        printf("1.3");
+    }
+    else if (strcmp(grade,"D0")==0) {
+        printf("1.0");
+    }
+    else if (strcmp(grade,"D-")==0) {
+        printf("0.7");
+    }
+    else {
+        printf("0.0");
+    }
+    return 0;
+}
