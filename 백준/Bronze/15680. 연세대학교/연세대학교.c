52371efa95fec3816c5dffafcc618aed9716d78e
jpzkskep <jpzkskep@gmail.com>
2025-10-25 14:11:34 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int input;
+    scanf("%d",&input);
+    if (input==1) {
+        printf("Leading the Way to the Future");
+    }
+    if (input==0) {
+        printf("YONSEI");
+    }
+    return 0;
+}
