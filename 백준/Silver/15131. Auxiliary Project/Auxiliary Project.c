cd56926cca19b51b4aec1f6d147b07eb65c38ba5
jpzkskep <jpzkskep@gmail.com>
2025-11-12 07:07:44 +0900

    [Silver III] Title: Auxiliary Project, Time: 4 ms, Memory: 8804 KB -BaekjoonHub

---
+++ 백준/Silver/15131. Auxiliary Project/Auxiliary Project.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int n;
+    long long dp[1000001];
+    int repeat_count=1;
+    int second_count=2;
+    
+    dp[2]=2;
+    dp[3]=7;
+    dp[4]=4;
+    
+    scanf("%d",&n);
+    
+    for (int i=5; i<=n; i++) {
+        
+        if (repeat_count==1) {
+            repeat_count++;
+            dp[i]=dp[i-2]+1;
+        }
+        else if (repeat_count==2) {
+            repeat_count++;
+            dp[i]=dp[3]*second_count;
+            second_count++;
+            
+        }
+        else {
+            repeat_count=1;
+            dp[i]=dp[i-3]+7;
+        }
+    }
+    printf("%lld",dp[n]);
+    return 0;
+}
